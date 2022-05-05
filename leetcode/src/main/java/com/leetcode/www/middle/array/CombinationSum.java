a2a1946f8c67f3c593ed31af3a01fd843f8d00dc
wangfengjia <fjym18262608918@gmail.com>
2022-05-05 16:13:32 +0800

    57th day,combination sum backtrack method

--- leetcode/src/main/java/com/leetcode/www/middle/array/CombinationSum.java
+++ leetcode/src/main/java/com/leetcode/www/middle/array/CombinationSum.java
@@ -1,6 +1,8 @@
 package com.leetcode.www.middle.array;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -42,6 +44,46 @@ public class CombinationSum {
         }
     }
 
+
+    /**
+     * 回溯加剪枝
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> solutionV2(int[] nums, int target){
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        if (n == 0){
+            return ans;
+        }
+
+        Deque<Integer> path = new ArrayDeque<>();
+        dfsV2(nums, n, 0, target, path, ans);
+        return ans;
+    }
+
+    private void dfsV2(int[] nums, int len, int begin, int target, Deque<Integer> path, List<List<Integer>> ans){
+
+        if (target < 0){
+            return;
+        }
+        if (target == 0){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        //在搜索过程中，每层的第二个节点开始,不能搜索同一层节点已经使用过nums里面的元素，这样才能够使得最后结果里面不出现重复列表
+        for (int i = begin; i < len; i++){
+            path.addLast(nums[i]);
+            //由于每个元素可以重复选择，所以下一轮搜索的起点还是i
+            dfsV2(nums, len, i, target - nums[i], path, ans);
+            //撤销选择
+            path.removeLast();
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] arr = new int[]{2,3,6,7};
@@ -50,6 +92,13 @@ public class CombinationSum {
         CombinationSum combinationSum = new CombinationSum();
         List<List<Integer>> lists = combinationSum.solution(arr, target);
         for (List<Integer> list : lists){
+            System.out.println(list);
+        }
+
+        System.out.println("=============================================");
+
+        List<List<Integer>> ans = combinationSum.solutionV2(arr, target);
+        for (List<Integer> list : ans){
             System.out.println(list);
         }
     }
